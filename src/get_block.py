1025d76b4f85145649d86b95d4c8b32508b0e3c8
John Davies <jd.other@hotmail.com>
2023-01-01 20:35:04 +0000

    added initial error handling to get_block

--- src/get_block.py
+++ src/get_block.py
@@ -7,19 +7,28 @@ USERNAME = os.environ["SSH_USER"]
 PASSWORD = os.environ["SSH_PASSWORD"]
 
 def lambda_handler(event, context): 
-    myhash = event['queryStringParameters']['hash']
+    input_hash = event['queryStringParameters']['hash']
     
     client = paramiko.client.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(HOST, username=USERNAME, password=PASSWORD)
-    _stdin, _stdout, _stderr = client.exec_command(f"bitcoin-cli -named getblock blockhash={myhash} verbosity=1")
-    response = _stdout.read().decode()
+    _stdin, _stdout, _stderr = client.exec_command(f"bitcoin-cli -named getblock blockhash={input_hash} verbosity=1")
+    response = _stdout.read().decode().strip()
+    error = _stderr.read().decode().strip()
     client.close()
     
     response_object = {}
-    response_object['statusCode'] = 200
     response_object['headers'] = {}
     response_object['headers']['Content-type'] = 'application/json'
-    response_object['body'] = json.dumps(response)
     
+    if response:
+        response_object['statusCode'] = 200
+        response_object['body'] = response
+    elif error:
+        response_object['statusCode'] = 502
+        response_object['body'] = error
+    else:
+        response_object['statusCode'] = 500
+        response_object['body'] = json.dumps({"Message": "Custom message - lambda failed"})
+        
     return response_object
