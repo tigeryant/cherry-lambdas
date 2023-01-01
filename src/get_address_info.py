ff549ce058770e38c228518907c4377beedc247e
John Davies <jd.other@hotmail.com>
2023-01-01 18:29:01 +0000

    removed comments and print statements from lambdas - cleanup

--- src/get_address_info.py
+++ src/get_address_info.py
@@ -6,24 +6,14 @@ HOST = os.environ["NODE_IP"]
 USERNAME = os.environ["SSH_USER"]
 PASSWORD = os.environ["SSH_PASSWORD"]
 
-def lambda_handler(event, context): 
-    # print(f"event queryStringParameters: {event['queryStringParameters']}")
-    # print(f"event: {event}")
-    
-    # print('event block hash: ', event['hash'])
-    # txid = event['hash']
-    # myhash = "00000000c937983704a73af28acdec37b049d214adbda81d7e2a3dd146f6ed09"
+def lambda_handler(event): 
     address = event['queryStringParameters']['address']
     
     client = paramiko.client.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    print('before connect')
     client.connect(HOST, username=USERNAME, password=PASSWORD)
-    print('after connect')
-    # _stdin, _stdout,_stderr = client.exec_command(f"bitcoin-cli -named getblock blockhash={myhash} verbosity=0")
-    _stdin, _stdout,_stderr = client.exec_command(f"bitcoin-cli getaddressinfo {address}")
+    _stdin, _stdout, _stderr = client.exec_command(f"bitcoin-cli getaddressinfo {address}")
     response = _stdout.read().decode()
-    # print('response: ', response)
     client.close()
     
     response_object = {}
@@ -32,21 +22,4 @@ def lambda_handler(event, context):
     response_object['headers']['Content-type'] = 'application/json'
     response_object['body'] = json.dumps(response)
     
-    # print('response object: ', response_object)
-    
     return response_object
-    
-    # print(f"event qsp: {event['queryStringParameters']}")
-    
-    # return {
-        # "statusCode": 200,
-        # "headers": {
-            # "Content-Type": "application/json"
-        # },
-        # 'body': json.dumps('Hello from Lambda!')
-        # "body": json.dumps({
-        #     "myBodyKey": "myBodyValue"
-        # })
-        # "body": json.dumps(response)
-        # "body": json.dumps(event['queryStringParameters'])
-    # }
